dbe8367b795da6e0694eff0b9c0407356f35ce9a
helangor <henrikki_helander@hotmail.com>
2020-04-14 18:55:05 +0300

    Added Header, Note and Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    const thisYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright ⓒ {thisYear}</p>
+        </footer>
+    )
+};
+
+export default Footer;
