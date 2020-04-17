48dd97cc1cd7c214f4c27037f950588bc3d30ad0
helangor <henrikki_helander@hotmail.com>
2020-04-17 17:11:58 +0300

    Added add and delete functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+import { createNotEmittedStatement } from "typescript";
+
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(noteItem =>
-                <Note
-                    key={noteItem.key}
-                    title={noteItem.title}
-                    content={noteItem.content}
-                />
-            )}
+            <CreateArea
+                onAdd={addNote}
+            />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
+            <Note key={1} title="Note title" content="Note content" />
             <Footer />
         </div>
+    );
+}
 
-    )
-};
-
-export default App
+export default App;
